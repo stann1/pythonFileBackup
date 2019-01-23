fc01e6154cd4c0358281e38a1504cda943240ebc
Stanislav Stoychev <stoychev.st2@gmail.com>
2019-01-23 11:51:13 +0200

    removed hardcoded paths; added delete functionality for old versions; other improvements

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+
 """
 archives files in a folder while preserving the last few versions
 """
@@ -14,24 +16,45 @@ def make_archive(source, destination):
         shutil.make_archive(name, format, archive_from, archive_to)
         shutil.move('%s.%s'%(name,format), destination)
 
+def delete_old_versions(fileList, numberToKeep, targetPath):
+        print('Deleting all but the last %s old versions...'%(numberToKeep))
+        fileList.sort()
+        for i in range(len(fileList) - numberToKeep):
+                print('Deleted ' + fileList[i])
+                os.remove(os.path.join(targetPath, fileList[i]))
+
 #print(sys.argv)
-targetPath = sys.argv[1] if len(sys.argv) >= 2 else "C:\\Games\\Steam\\userdata\\24593029"
-targetSubFolder = sys.argv[2] if len(sys.argv) >= 3 else "219740"
-archiveName = sys.argv[3] if len(sys.argv) >= 4 else targetSubFolder
+if len(sys.argv)<= 1:
+        raise Exception('Missing arguments for targetPath and targetSubfolder')
+
+DEFAULT_VERSIONS_TO_KEEP = 1 # 1 will keep only the current archive and 1 version back, 0 would mean current archive only
+
+targetPath = sys.argv[1]
+targetSubFolder = sys.argv[2]
+versionsToKeep = sys.argv[3] if len(sys.argv) >= 4 else DEFAULT_VERSIONS_TO_KEEP
+archiveName = sys.argv[4] if len(sys.argv) >= 5 else targetSubFolder    # name of the archive will be the name of the subfolder
 
 today = str(date.today())
 
 shouldMakeArchive = True
 targetArchiveName = '%s-%s.%s'%(archiveName,today,"zip") # eg. testarchive-2019-01-21.zip
 
+archivesToDelete = []
 filesAtTarget = os.listdir(targetPath)
 for f in filesAtTarget:
-        if os.path.isfile(os.path.join(targetPath, f)) and f == targetArchiveName :
-                shouldMakeArchive = False
-                break
+        if os.path.isfile(os.path.join(targetPath, f)):
+                if  f == targetArchiveName :
+                        shouldMakeArchive = False
+                elif archiveName in f :
+                        archivesToDelete.append(f)
 
 
 if shouldMakeArchive:
         make_archive(os.path.join(targetPath, targetSubFolder), os.path.join(targetPath, targetArchiveName))
 else:
         print("archive already exists")
+
+if len(archivesToDelete) > 0 and len(archivesToDelete) > versionsToKeep :
+        delete_old_versions(archivesToDelete, 1, targetPath)
+else:
+        print("No old version deleted")
